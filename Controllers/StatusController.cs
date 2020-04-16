c6bd1362ac1c02e814a6f03ddc5b7e9e4968767c
Matt Bird <matt@birdhq.net>
2020-04-16 17:40:31 +1000

    Added Status Endpoint

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CertifyWPF.Controllers
+{
+    [Authorize]
+    public class StatusController : ApiController
+    {
+        public string Get()
+        {
+            return "Status: OK";
+        }
+    }
+}
