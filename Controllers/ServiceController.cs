5c38b85c07ee3f3419c90d4025d4abdda2bd182c
Matt Bird <matt@birdhq.net>
2020-04-19 07:39:29 +1000

    Added Service endpoint

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,42 @@
+﻿using CertifyWPF.WPF_Service;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace CertifyWPF.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        // GET: api/Service
+        public IEnumerable<string> Get()
+        {
+            return Service.getServiceList();
+        }
+
+        // GET: api/Service/5
+        public Service Get(long id)
+        {
+            if (id != -1)
+            {
+                Service service = new Service(id);
+                if (!String.IsNullOrEmpty(service.name)) return service;
+            }
+            return null;
+        }
+
+        // POST: api/Service
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Service/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Service/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
